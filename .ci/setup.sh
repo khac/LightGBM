b00c994b9b1af60b674bc6a867de5a81c3293ddd
Nikita Titov <nekit94-08@mail.ru>
2024-10-05 23:03:57 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -7,7 +7,7 @@ export OS_NAME="macos"
 
 brew install lightgbm
 
-lightgbm config="../examples/regression/train.conf"
+lightgbm config="./examples/regression/train.conf"
 
 # if [[ $OS_NAME == "macos" ]]; then
 #     if  [[ $COMPILER == "clang" ]]; then
