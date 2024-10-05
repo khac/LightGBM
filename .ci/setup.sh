a698e1255de7a0886a34a8a347532b944102ef75
Nikita Titov <nekit94-08@mail.ru>
2024-10-06 00:47:52 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -3,10 +3,11 @@
 set -e -E -u -o pipefail
 
 brew install cmake
+brew install ninja
 
 git clone --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
-cmake -B build -S . -DUSE_OPENMP=OFF
+cmake -B build -S . -DUSE_OPENMP=OFF -G Ninja
 cmake --build build -j4
 
 ls
