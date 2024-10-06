1eb24e6c8f9dede2c4b1d217e827ae596bc661e7
Nikita Titov <nekit94-08@mail.ru>
2024-10-06 20:23:07 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -9,8 +9,6 @@ export PATH="/usr/local/opt/openjdk/bin:$PATH"
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 export JAVA_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk
-JAVA_HOME=$(brew --prefix openjdk@11)
-export JAVA_HOME=/usr/local/opt/openjdk
 export JAVA_HOME="/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home/"
 export CPPFLAGS="-I/usr/local/opt/openjdk/include"
 export CFLAGS="-I/usr/local/opt/openjdk/include"
