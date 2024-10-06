7077285476901b9215d81601a4fb0c2cebb6bbff
Nikita Titov <nekit94-08@mail.ru>
2024-10-06 20:04:11 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -8,6 +8,7 @@ brew install cmake openjdk swig libomp
 export PATH="/usr/local/opt/openjdk/bin:$PATH"
 sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 export JAVA_HOME=/usr/local/opt/openjdk
+export JAVA_HOME=/opt/homebrew/opt/openjdk/libexec/openjdk.jdk
 export CPPFLAGS="-I/usr/local/opt/openjdk/include"
 export CFLAGS="-I/usr/local/opt/openjdk/include"
 export LDFLAGS="-I/usr/local/opt/openjdk/include"
