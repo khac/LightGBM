720f86d883ce7cd906d38290af641f7beb65c532
Nikita Titov <nekit94-08@mail.ru>
2024-10-06 21:58:19 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -7,8 +7,7 @@ sudo rm -rf /Library/Java/JavaVirtualMachines/*
 
 brew install cmake openjdk swig libomp
 
-echo "$(brew --prefix openjdk)"
-export JAVA_HOME="/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/"
+export JAVA_HOME="$(brew --prefix openjdk)/libexec/openjdk.jdk/Contents/Home/"
 
 git clone --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
